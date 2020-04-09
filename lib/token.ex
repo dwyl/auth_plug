de2bb2fd76141cf8720bfcd2c4b3c81f79dd3c11
nelsonic <nelson+github@dwyl.io>
2020-04-09 19:51:10 +0100

    create lib/token.ex to use Joken. kinda stupid that we cant just use it directly ... 🙄

---
+++ lib/token.ex
@@ -0,0 +1,8 @@
+defmodule AuthPlug.Token do
+  @moduledoc """
+  Token module to create and validate jwt.
+  see https://hexdocs.pm/joken/configuration.html#module-approach
+
+  """
+  use Joken.Config
+end
