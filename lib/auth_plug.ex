5d819f729c0f27f7ae0b9fe2eec95c8065a73a31
nelsonic <nelson+github@dwyl.io>
2020-04-21 15:45:31 +0100

    remove excess IO.inspect instances #2

--- lib/auth_plug.ex
+++ lib/auth_plug.ex
@@ -9,15 +9,6 @@ defmodule AuthPlug do
   end
 
   def call(conn, _params) do
-    # IO.inspect(conn, labels: "conn:13")
-    # IO.inspect(params, label: "params:14")
-    IO.inspect(conn.query_string, label: "conn.query_string:15")
-    IO.inspect(conn.query_string =~ "jwt", label: "=~")
-    # IO.inspect(conn.assigns, label: "conn.assigns:17")
-    # IO.inspect(conn.assigns.person, label: "conn.assigns.person:18")
-    # query = URI.decode_query(conn.query_string)
-    # IO.inspect(query, label: "query")
-    # IO.inspect(get_req_header(conn, "authorization"), label: "21")
 
     # Setup Plug.Session
     conn = setup_session(conn)
@@ -27,18 +18,15 @@ defmodule AuthPlug do
 
       #  Check for Person in Plug.Conn.assigns
       Map.has_key?(conn.assigns, :person) && not is_nil(conn.assigns.person) ->
-        # IO.inspect(conn.assigns.person, label: "conn.assigns.person")
         conn.assigns.person
 
       # Check for Session in Plug.Session:
       not is_nil(get_session(conn, :person)) ->
-        # IO.inspect(get_session(conn, :person), label: "get_session(conn, :person)")
         get_session(conn, :person)
 
       # Check for JWT in URL Query String:
       conn.query_string =~ "jwt" ->
         query = URI.decode_query(conn.query_string)
-        # IO.inspect(query, label: "query")
         Map.get(query, "jwt")
 
       # Check for JWT in Headers:
@@ -47,14 +35,10 @@ defmodule AuthPlug do
           |> List.keyfind("authorization", 0)
           |> get_token_from_header()
 
-
       # By default return nil so auth check fails
       true ->
-        IO.inspect("cond true:54")
         nil
-
     end
-    IO.inspect(jwt, label: "jwt:31")
     validate_token(conn, jwt)
   end
 
@@ -98,9 +82,6 @@ defmodule AuthPlug do
   defp validate_token(conn, nil), do: unauthorized(conn)
 
   defp validate_token(conn, jwt) do
-    IO.inspect(jwt, label: "jwt:102")
-    IO.inspect(@signer, label: "@signer:103")
-    IO.inspect(AuthPlug.Token.verify(jwt, @signer), label: "verify:104")
     case AuthPlug.Token.verify_and_validate(jwt, @signer) do
       {:ok, values} ->
         # convert map of string to atom: stackoverflow.com/questions/31990134
@@ -115,10 +96,6 @@ defmodule AuthPlug do
         unauthorized(conn)
     end
   end
-
-  # def extract(conn, params) do
-  #
-  # end
 
   defp unauthorized(conn) do
     conn
