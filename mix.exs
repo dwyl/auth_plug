3bcdac1517b97fc565b20508addf27ddd26866bc
nelsonic <nelson+github@dwyl.io>
2020-04-29 17:23:02 +0100

    bump to v1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AuthPlug.MixProject do
   def project do
     [
       app: :auth_plug,
-      version: "0.16.0",
+      version: "1.0.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
