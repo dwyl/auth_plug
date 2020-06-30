35b44a425e35a2dad7b9fb82405e3a2c8b2b9ad3
Tom Haines <tom@tomhaines.xyz>
2020-06-30 15:51:48 +0100

    Refractored plug

--- lib/demo/application.ex
+++ lib/demo/application.ex
@@ -4,15 +4,16 @@ defmodule AuthPlug.Application do
   use Application
   require Logger
   @data %{email: "alexa@gmail.com", name: "Alexa"}
-  @jwt AuthPlug.Token.generate_jwt!(@data)
 
   def start(_type, _args) do
     children = [
       {Plug.Cowboy, scheme: :http, plug: AuthPlug.Router, options: [port: 4000]}
     ]
 
+    jwt = AuthPlug.Token.generate_jwt!(@data)
+
     Logger.info("First visit: http://localhost:4000/admin")
-    Logger.info("Then visit: http://localhost:4000/admin?jwt=" <> @jwt)
+    Logger.info("Then visit: http://localhost:4000/admin?jwt=" <> jwt)
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: App.Supervisor]
