401515f930d6ce797af8b47001051d3460ebc576
nelsonic <nelson+github@dwyl.io>
2020-08-05 12:39:41 +0100

    add test for AuthPlug.Helpers.check_environment_vars/0 as discussed in https://github.com/dwyl/auth_plug/pull/21#issuecomment-652365778

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -30,7 +30,7 @@ defmodule AuthPlug.Helpers do
   def check_environment_vars do
     key = AuthPlug.Token.api_key()
     if is_nil(key) do
-      raise "No AUTH_API_KEY set, find out how at https://github.com/dwyl/auth_plug#2-get-your-auth_api_key-"
+      raise "No AUTH_API_KEY set, find out how at: https://git.io/JJ6sS"
     end
   end
 end
