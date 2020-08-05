49c30eafa729849adfd01d955f7a517cee04f142
nelsonic <nelson+github@dwyl.io>
2020-08-05 12:41:18 +0100

    bump version 1.1.1 > 1.2.0 as AuthPlug.Helpers.check_environment_vars/0 raises error https://github.com/dwyl/auth_plug/pull/21

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AuthPlug.MixProject do
   def project do
     [
       app: :auth_plug,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
