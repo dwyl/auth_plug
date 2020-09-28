41b3c371ee5a746de41ac54364f2cd6e2667ef28
nelsonic <nelson+github@dwyl.io>
2020-09-28 21:42:52 +0100

    implment Optional Auth for #27

---
+++ lib/auth_plug_optional.ex
@@ -0,0 +1,36 @@
+defmodule AuthPlugOptional do
+  @moduledoc """
+  `AuthPlugOptional` handles any route where authentication is optional
+  e.g. if you're building a CMS where content is public
+  but seeing comments requires auth, the same page can show different
+  content depending on if the person has logged in or not.
+  Please see `README.md` for for optional auth usage.
+  """
+  require Logger
+
+  @doc """
+  `init/1` returns options unmodified
+  """
+  def init(options), do: options
+
+  @doc """
+  `call/2` is invoked to handle each HTTP request which `auth_plug` protects.
+  If the `conn` contains a valid JWT in Authentication Headers, URL or Cookie,
+  then continue to the optionally protected route,
+  else redirect to the `auth_url` with the referer set as the continuation URL.
+  NOTE: this plug does NOT set the HTTP Status on a request, that is left to you.
+  """
+  def call(conn, _options) do
+    jwt = AuthPlug.Token.get_jwt(conn)
+
+    case AuthPlug.Token.verify_jwt(jwt) do
+      # Valid JWT, assign the session data and return conn:
+      {:ok, values} ->
+        AuthPlug.Token.put_current_token(conn, jwt, values)
+
+      # Don't do anything as there is no valid JWT and Auth is Optional
+      {:error, _reason} ->
+        conn
+    end
+  end
+end
---
+++ test/auth_plug_optional_test.exs
@@ -0,0 +1,42 @@
+defmodule AuthPlugOptionalTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  alias AuthPlug.Token
+  @opts AuthPlugOptional.init(%{})
+
+  test "init/1 returns options unmodified" do
+    assert AuthPlugOptional.init(%{}) == %{}
+  end
+
+  describe "test admin endpoint" do
+    setup %{endpoint: endpoint} do
+      test_conn =
+        conn(:get, endpoint)
+        |> init_test_session(%{})
+
+      {:ok, conn: test_conn}
+    end
+
+    @tag endpoint: "/optional"
+    test "No Error if JWT is not defined", %{conn: conn} do
+      conn = AuthPlugOptional.call(conn, @opts)
+      # AuthPlugOptional does NOT set the HTTP status code:
+      assert conn.status == nil
+      # nothing is set on conn.assigns
+      assert conn.assigns == %{}
+    end
+
+    @tag endpoint: "/optional"
+    test "Conn.assign.person (the verified JWT)", %{conn: conn} do
+      data = %{email: "alex@dwyl.com", name: "Alex"}
+      jwt = Token.generate_jwt!(data)
+
+      conn =
+        conn
+        |> assign(:jwt, jwt)
+        |> AuthPlugOptional.call(%{})
+
+      assert conn.assigns.person.email == "alex@dwyl.com"
+    end
+  end
+end
