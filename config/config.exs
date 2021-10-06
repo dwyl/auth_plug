c669b22c156c31705e6d1c0be88fb81296f859fc
nelsonic <nelson+github@dwyl.com>
2021-10-06 23:38:46 +0100

    update Elixir & OTP versions in .travis.yml #29

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :fields, key: :value
+#     config :auth_plug, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:fields, :key)
+#     Application.get_env(:auth_plug, :key)
 #
 # You can also configure a 3rd-party app:
 #
