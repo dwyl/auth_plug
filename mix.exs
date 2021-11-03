65cdeaf8ea7198359d055e3b9ea27645a73e3a99
nelsonic <nelson+github@dwyl.com>
2021-11-03 21:34:06 +0000

    bump version to 1.3.4 to release the logout/1 function/feature #11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AuthPlug.MixProject do
   def project do
     [
       app: :auth_plug,
-      version: "1.3.3",
+      version: "1.3.4",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
